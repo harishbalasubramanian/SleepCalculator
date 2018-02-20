3743ee9f3cf4ea9397b86bf59cc8bfd2f9da8b95
harishbalasubramanian <harishbala153@gmail.com>
2018-02-19 21:08:26 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*Author: Harish B
+2/19/18
+Feel Free to Fork and Extend
+But remember to copy and paste this comment*/
+import javax.swing.JFrame;
+public class one {
+
+	public static void main(String[] args) {
+		two to = new two();
+		to.setSize(600, 600);
+		to.setVisible(true);
+		to.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
